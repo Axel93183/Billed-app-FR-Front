c7dd327662313585864514304faea4f0c5455d48
Axel GMX <agmx183@gmail.com>
2024-04-30 19:53:52 +0200

    Tests unitaire (statement) : NewBill.js (79.25%)

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,44 +2,130 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import { handleChangeFile } from "../containers/NewBill.js";
-import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
-
+import { ROUTES_PATH } from "../constants/routes.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then I should see the NewBill page", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      expect(screen.getByTestId("form-new-bill")).toBeTruthy()
+    let bill
+
+    document.body.innerHTML = `
+      <div>
+        <form data-testid="form-new-bill"></form>
+        <input data-testid="file">
+      </div>
+    `
+    const onNavigate = jest.fn()
+
+    const store = {
+      bills: jest.fn(() => ({
+        create: jest.fn().mockResolvedValue({
+          fileUrl: "testUrl",
+          key: "testKey",
+        }),
+        update: jest.fn().mockResolvedValue({}),
+      })),
+    }
+
+    const localStorage = window.localStorage
+    localStorage.setItem("user", JSON.stringify({ email: "test@test.com" }))
+
+    beforeEach(() => {
+      bill = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage,
+      })
     })
-    test("Then the file input should accept only jpg, jpeg, and png files", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      const input = screen.getByTestId("file");
-      input.addEventListener('change', handleChangeFile);
-      const file = new File([''], 'test.jpg', { type: 'image/jpeg' });
-      const event = new Event('change');
-      Object.defineProperty(event, 'target', { value: { files: [file] } });
-      input.dispatchEvent(event);
-      const errorMessage = document.querySelector('.error-file-extension');
-      expect(errorMessage).toBeNull();
-    });
-    test("Then an error message should appear if the file extension is not allowed", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      const input = screen.getByTestId("file");
-      input.addEventListener('change', handleChangeFile);
-      const file = new File([''], 'test.txt', { type: 'text/plain' });
-      const event = new Event('change');
-      Object.defineProperty(event, 'target', { value: { files: [file] } });
-      input.dispatchEvent(event);
-      const parentContainer = input.parentNode;
-      const errorMessage = parentContainer.querySelector('.error-file-extension');
-      expect(errorMessage).toBeDefined();
-    });
-    
+
+    test("Then I should create an instance of NewBill", () => {
+      expect(bill).toBeInstanceOf(NewBill)
+    })
+
+    test("Then I should initialize the NewBill instance with correct properties", () => {
+      expect(bill.document).toEqual(document)
+      expect(bill.onNavigate).toEqual(onNavigate)
+      expect(bill.store).toEqual(store)
+      expect(bill.fileUrl).toBeNull()
+      expect(bill.fileName).toBeNull()
+      expect(bill.billId).toBeNull()
+    })
+
+    test("Then I should handle file change correctly when file extension is jpg", async () => {
+      const allowedFile = new File(["file"], "test.jpg", { type: "image/jpg" })
+
+      const inputElement = document.querySelector(`input[data-testid="file"]`)
+      Object.defineProperty(inputElement, "files", {
+        value: [allowedFile],
+      })
+
+      const event = {
+        preventDefault: jest.fn(),
+        target: {
+          value: "C:\\testpath\\test.jpg",
+          files: [allowedFile],
+        },
+      }
+
+      await bill.handleChangeFile(event)
+
+      expect(bill.fileUrl).toBeDefined()
+      expect(bill.fileName).toBe("test.jpg")
+    })
+
+    test("Then an error message should appear if the file extension is not allowed", async () => {
+      const mockFile = new File(["file"], "file.txt", { type: "text/plain" })
+
+      const inputElement = document.querySelector(`input[data-testid="file"]`)
+      if (!inputElement.files) {
+        Object.defineProperty(inputElement, "files", {
+          value: [mockFile],
+        })
+      }
+
+      const mockEvent = {
+        preventDefault: jest.fn(),
+        target: {
+          value: "C:\\filepath\\file.txt",
+          files: [mockFile],
+        },
+      }
+
+      await bill.handleChangeFile(mockEvent)
+
+      const errorMessage = document.querySelector(".error-file-extension")
+      expect(errorMessage).toBeDefined()
+
+      expect(inputElement.value).toEqual("")
+    })
+
+    test("Then I should navigate to ROUTES_PATH['Bills'] after form submission", async () => {
+      document.body.innerHTML = `
+      <div>
+        <form data-testid="form-new-bill">
+          <select data-testid="expense-type"></select>
+          <input data-testid="expense-name">
+          <input data-testid="amount">
+          <input data-testid="datepicker">
+          <input data-testid="vat">
+          <input data-testid="pct">
+          <textarea data-testid="commentary"></textarea>
+        </form>
+        <input data-testid="file">
+      </div>
+    `
+      const formElement = document.querySelector(
+        `form[data-testid="form-new-bill"]`
+      )
+      const mockEvent = {
+        preventDefault: jest.fn(),
+        target: formElement,
+      }
+
+      await bill.handleSubmit(mockEvent)
+
+      expect(bill.onNavigate).toHaveBeenCalledWith(ROUTES_PATH["Bills"])
+    })
   })
 })
